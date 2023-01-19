0c599fe545131a0f537d75408414aafa512f5b8c
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-01-19 16:45:30 -0300

    iniciando t2

---
+++ t2/so.c
@@ -0,0 +1,381 @@
+#include "so.h"
+#include "tela.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PROGRAMAS 5
+
+typedef struct escalonador_t{
+  
+}esc_t;
+
+typedef struct programa_t{
+  int size;
+  int* code;
+}prog_t;
+
+struct so_t {
+  contr_t *contr;       // o controlador do hardware
+  bool paniquei;        // apareceu alguma situação intratável
+  cpu_estado_t *cpue;   // cópia do estado da CPU
+  tab_t* tabela;
+  prog_t** programs;
+  int proc_count;
+  int curr_prog;
+};
+
+struct process_info {
+  mem_t* mem;
+  cpu_estado_t* cpue;
+  int id;
+  int estado;
+  int disp;
+  acesso_t acesso; 
+};
+
+//declaracoes
+void so_atualiza_tab(so_t* self, int motivo);
+
+struct tabela_proc {
+  int n_procs;
+  proc_t** processes;
+};
+
+// funções auxiliares
+static void init_mem(so_t *self);
+static void panico(so_t *self);
+
+// processo 0 - init
+void init_proc(so_t* self) {
+  // inicializando memoria com o programa
+  mem_t* mem = mem_cria(MEM_TAM);
+  for (int i = 0; i < self->programs[0]->size; i++) {
+    mem_escreve(mem, i, self->programs[0]->code[i]);
+  }
+  // inicializando cpue
+  cpu_estado_t* cpu = cpue_cria();
+  int id = self->proc_count;
+  self->proc_count++;
+  int estado = EM_EXECUCAO;
+
+  // salvando na tabela
+  self->tabela->processes[0] = (proc_t*)malloc(sizeof(proc_t));
+  self->tabela->processes[0]->id = id;
+  self->tabela->processes[0]->estado = estado;
+  self->tabela->processes[0]->cpue = cpu;
+  self->tabela->processes[0]->mem = mem;
+}
+
+so_t *so_cria(contr_t *contr)
+{
+  so_t *self = malloc(sizeof(*self));
+  if (self == NULL) return NULL;
+  self->contr = contr;
+  self->paniquei = false;
+  self->cpue = cpue_cria();
+  self->proc_count = 0;
+  self->curr_prog = 0;
+  // coloca a CPU em modo usuário
+  /*
+  exec_copia_estado(contr_exec(self->contr), self->cpue);
+  cpue_muda_modo(self->cpue, usuario);
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+  */
+
+  self->tabela = (tab_t*) malloc (sizeof(tab_t));
+  self->tabela->processes = (proc_t**) malloc (sizeof(proc_t*) * PROGRAMAS);
+  self->programs = (prog_t**) malloc (sizeof(prog_t*) * PROGRAMAS);
+
+  int progr0[] = {
+    #include "init.maq"
+  };
+  self->programs[0] = (prog_t*) malloc (sizeof(prog_t));
+  self->programs[0]->size = sizeof(progr0)/sizeof(int);
+  self->programs[0]->code = (int*) malloc (sizeof(int) * self->programs[0]->size);
+  for (int i = 0; i < self->programs[0]->size; i++) {
+    self->programs[0]->code[i] = progr0[i];
+  }
+
+  // programa para executar na nossa CPU
+  int progr1[] = {
+    #include "p1.maq"
+  };
+  self->programs[1] = (prog_t*) malloc (sizeof(prog_t));
+  self->programs[1]->size = sizeof(progr1)/sizeof(int);
+  self->programs[1]->code = (int*) malloc (sizeof(int) * self->programs[1]->size);
+  for (int i = 0; i < self->programs[1]->size; i++) {
+    self->programs[1]->code[i] = progr1[i];
+  }
+
+  int progr2[] = {
+    #include "p2.maq"
+  };
+  self->programs[2] = (prog_t*) malloc (sizeof(prog_t));
+  self->programs[2]->size = sizeof(progr2)/sizeof(int);
+  self->programs[2]->code = (int*) malloc (sizeof(int) * self->programs[2]->size);
+  for (int i = 0; i < self->programs[2]->size; i++) {
+    self->programs[2]->code[i] = progr2[i];
+  }
+
+  init_proc(self);
+  init_mem(self);
+  return self;
+}
+
+void so_destroi(so_t *self)
+{
+  cpue_destroi(self->cpue);
+  free(self);
+}
+
+
+static void so_trata_sisop_le(so_t *self)
+{
+  int disp = cpue_A(self->cpue);
+  int val;
+  err_t err = es_le(contr_es(self->contr), disp, &val);
+
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+
+  if (err != ERR_OK) {
+    int idx = self->curr_prog;
+    self->tabela->processes[idx]->acesso = leitura;
+    self->tabela->processes[idx]->disp = disp;
+    cpue_muda_erro(self->cpue, err, 0);
+
+  } else{
+    cpue_muda_X(self->cpue, val);
+  }
+
+  cpue_muda_A(self->cpue, err);
+  cpue_muda_PC(self->cpue, cpue_PC(self->cpue) + 2);
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+}
+
+
+static void so_trata_sisop_escr(so_t *self)
+{
+  int disp = cpue_A(self->cpue);
+  int val = cpue_X(self->cpue);
+  err_t err = es_escreve(contr_es(self->contr), disp, val);
+
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+
+  if (err != ERR_OK) {
+    int idx = self->curr_prog;
+    self->tabela->processes[idx]->disp = disp;
+    self->tabela->processes[idx]->acesso = escrita;
+    cpue_muda_erro(self->cpue, err, 0);
+  
+  }
+
+  cpue_muda_A(self->cpue, err);
+  cpue_muda_PC(self->cpue, cpue_PC(self->cpue) + 2);
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+}
+
+
+int esc_seleciona_processo(so_t* self) {
+  int idx;
+  for (idx = 0; idx < PROGRAMAS; idx++) {
+    if(self->tabela->processes[idx] != NULL && self->tabela->processes[idx]->estado == PRONTO) {
+      return idx;
+    }
+  }
+  return -1;
+}
+
+void so_atualiza_tab(so_t* self, int motivo) {
+  int idx_current = self->curr_prog;
+  cpue_copia(self->cpue, self->tabela->processes[idx_current]->cpue);
+  mem_t* mem_atual = contr_mem(self->contr);
+  for (int i = 0; i < MEM_TAM; i++) {
+    int valor;
+    mem_le(mem_atual, i, &valor);
+    mem_escreve(self->tabela->processes[idx_current]->mem, i, valor);
+  }
+  self->tabela->processes[idx_current]->estado = motivo;
+}
+
+void so_bota_proc(so_t* self) {
+  int idx = esc_seleciona_processo(self);
+
+  if (idx != -1) {
+    t_printf("PROGRAMA %d SENDO EXECUTADO!", idx);
+    self->tabela->processes[idx]->estado = EM_EXECUCAO;
+    self->curr_prog = idx;
+    cpue_copia(self->tabela->processes[idx]->cpue, self->cpue);
+    
+    mem_t *mem = contr_mem(self->contr);
+    for (int i = 0; i < mem_tam(self->tabela->processes[idx]->mem); i++) {
+      int val;
+      mem_le(self->tabela->processes[idx]->mem, i, &val);
+      mem_escreve(mem, i, val);
+    }
+    cpue_muda_modo(self->cpue, usuario);
+  }
+  else {
+    // botar cpu em zumbi
+    self->curr_prog = -1;
+    cpue_muda_modo(self->cpue, zumbi);
+  }
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+}
+
+
+// chamada de sistema para término do processo
+static void so_trata_sisop_fim(so_t *self)
+{
+  int id;
+  int i = self->curr_prog;
+  self->tabela->processes[i]->estado = FINALIZADO;
+  id = self->tabela->processes[i]->id;
+  t_printf("PROCESSO %d FINALIZADO", id);
+  so_bota_proc(self);
+
+  // interrupção da cpu foi atendida
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+   // incrementa o PC
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+  
+}
+
+
+// chamada de sistema para criação de processo
+static void so_trata_sisop_cria(so_t *self)
+{
+  int prog_number = cpue_A(self->cpue);   
+  // inicializando memoria com o programa
+  mem_t* mem = mem_cria(MEM_TAM);
+  for (int i = 0; i < self->programs[prog_number]->size; i++) {
+    mem_escreve(mem, i, self->programs[prog_number]->code[i]);
+  }
+  // inicializando cpue
+  cpu_estado_t* cpu = cpue_cria();
+  int id = self->proc_count;
+  self->proc_count++;
+  int estado = PRONTO;
+
+  // salvando na tabela
+  self->tabela->processes[id] = (proc_t*)malloc(sizeof(proc_t));
+  self->tabela->processes[id]->id = id;
+  self->tabela->processes[id]->estado = estado;
+  self->tabela->processes[id]->cpue = cpu;
+  self->tabela->processes[id]->mem = mem;
+
+  // interrupção da cpu foi atendida
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+   // incrementa o PC
+  cpue_muda_PC(self->cpue, cpue_PC(self->cpue)+2);
+  // altera o estado da CPU (deveria alterar o estado do processo)
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+
+  t_printf("PROCESSO %d CRIADO", id);
+}
+
+// trata uma interrupção de chamada de sistema
+static void so_trata_sisop(so_t *self)
+{
+  // o tipo de chamada está no "complemento" do cpue
+  exec_copia_estado(contr_exec(self->contr), self->cpue);
+  so_chamada_t chamada = cpue_complemento(self->cpue);
+  switch (chamada) {
+    case SO_LE:
+      so_trata_sisop_le(self);
+      break;
+    case SO_ESCR:
+      so_trata_sisop_escr(self);
+      break;
+    case SO_FIM:
+      so_trata_sisop_fim(self);
+      break;
+    case SO_CRIA:
+      so_trata_sisop_cria(self);
+      break;
+    default:
+      t_printf("so: chamada de sistema nao reconhecida %d\n", chamada);
+      panico(self);
+      break;
+  }
+}
+
+// trata uma interrupção de tempo do relógio
+static void so_trata_tic(so_t *self)
+{
+  // TODO: tratar a interrupção do relógio
+  // VERIFICAR LEITURA E ESCRITA
+  for (int i = 0; i < PROGRAMAS; i++) {
+    if (self->tabela->processes[i] != NULL && self->tabela->processes[i]->estado == BLOCKEADO) {
+      int disp = self->tabela->processes[i]->disp;
+      acesso_t forma = self->tabela->processes[i]->acesso;
+      if(es_pronto(contr_es(self->contr), disp, forma)) {
+        self->tabela->processes[i]->estado = PRONTO;
+        if (cpue_modo(self->cpue) == zumbi) {
+          cpue_muda_modo(self->cpue, supervisor);
+          cpue_muda_erro(self->cpue, ERR_OK, 0);
+          exec_altera_estado(contr_exec(self->contr), self->cpue);
+        }
+      }
+    }
+  }
+  if (self->curr_prog == -1) {
+    so_bota_proc(self);
+    cpue_muda_erro(self->cpue, ERR_OK, 0);
+    exec_altera_estado(contr_exec(self->contr), self->cpue);
+  }
+}
+
+void so_trata_disp(so_t* self) {
+  so_atualiza_tab(self, BLOCKEADO);
+  so_bota_proc(self);
+  cpue_muda_erro(self->cpue, ERR_OK, 0);
+  exec_altera_estado(contr_exec(self->contr), self->cpue);
+}
+
+
+// houve uma interrupção do tipo err — trate-a
+void so_int(so_t *self, err_t err)
+{
+  switch (err) {
+    case ERR_SISOP:
+      so_trata_sisop(self);
+      break;
+    case ERR_TIC:
+      so_trata_tic(self);
+      break;
+    // DISP OCUPADO
+    case ERR_OCUP:
+      so_trata_disp(self);
+      break;
+    default:
+      t_printf("SO: interrupção nao tratada [%s]", err_nome(err));
+      self->paniquei = true;
+  }
+}
+
+// retorna false se o sistema deve ser desligado
+bool so_ok(so_t *self)
+{
+  return !self->paniquei;
+}
+
+
+// carrega um programa na memória
+static void init_mem(so_t *self)
+{
+  // inicializa a memória com o programa 
+  mem_t *mem = contr_mem(self->contr);
+  for (int i = 0; i < self->programs[0]->size; i++) {
+    if (mem_escreve(mem, i, self->programs[0]->code[i]) != ERR_OK) {
+      t_printf("so.init_mem: erro de memoria, endereco %d\n", i);
+      panico(self);
+    }
+  }
+}
+  
+static void panico(so_t *self) 
+{
+  t_printf("Problema irrecuperavel no SO");
+  self->paniquei = true;
+}
---
+++ t2/so.h
@@ -0,0 +1,41 @@
+#ifndef SO_H
+#define SO_H
+
+// SO
+// Simula o sistema operacional
+// É chamado na inicialização (quando deve colocar um programa na memória), e
+//   depois só quando houver interrupção
+//
+// Por enquanto não faz quase nada
+#define PRONTO 0
+#define EM_EXECUCAO 1
+#define FINALIZADO 2
+#define BLOCKEADO 3
+
+typedef struct so_t so_t;
+typedef struct tabela_proc tab_t;
+typedef struct process_info proc_t;
+
+// as chamadas de sistema
+typedef enum {
+  SO_LE = 1,       // lê do dispositivo em A; coloca valor lido em X
+  SO_ESCR,         // escreve o valor em X no dispositivo em A
+  SO_FIM,          // encerra a execução do processo
+  SO_CRIA,         // cria um novo processo, para executar o programa A
+} so_chamada_t;
+
+#include "contr.h"
+#include "err.h"
+#include <stdbool.h>
+
+so_t *so_cria(contr_t *contr);
+
+void so_destroi(so_t *self);
+
+// houve uma interrupção do tipo err — trate-a
+void so_int(so_t *self, err_t err);
+
+// retorna false se o sistema deve ser desligado
+bool so_ok(so_t *self);
+
+#endif // SO_H
